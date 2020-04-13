d2e0595b07b80e252e347bc917204548eb33f267
rjx <2247359268@qq.com>
2020-04-13 14:29:37 +0800

    配置swagger的参数说明，在实体类上面添加了说明，这样相当于是一个注释了

--- src/main/java/com/zgy/learn/bootswaggermailquartzmongo/pojo/Student.java
+++ src/main/java/com/zgy/learn/bootswaggermailquartzmongo/pojo/Student.java
@@ -1,19 +1,26 @@
 package com.zgy.learn.bootswaggermailquartzmongo.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "学生的实体类", description = "学生的实体类")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
-
+    @ApiModelProperty(value = "学生id", notes = "学生id")
     private int id;
+    @ApiModelProperty(value = "学生姓名", notes = "学生姓名")
     private String name;
+    @ApiModelProperty(value = "学生性别", notes = "学生性别")
     private String gender;
     // 年级
+    @ApiModelProperty(value = "学生姓名", notes = "学生姓名")
     private String grade;
     // 班级
+    @ApiModelProperty(value = "学生班级", notes = "学生班级")
     private String classes;
 }
