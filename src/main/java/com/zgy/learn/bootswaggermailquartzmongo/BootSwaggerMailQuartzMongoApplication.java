91d1a665761dc2c624a4c4494389c4569cefd795
rjx <2247359268@qq.com>
2020-04-13 11:33:19 +0800

    搭建项目框架

---
+++ src/main/java/com/zgy/learn/bootswaggermailquartzmongo/BootSwaggerMailQuartzMongoApplication.java
@@ -0,0 +1,15 @@
+package com.zgy.learn.bootswaggermailquartzmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+public class BootSwaggerMailQuartzMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSwaggerMailQuartzMongoApplication.class, args);
+    }
+
+}
