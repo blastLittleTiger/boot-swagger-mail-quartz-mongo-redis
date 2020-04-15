fe019dcc70cf2700721b950378e7b52e8149ead5
zuiguangyin <2247359268@qq.com>
2020-04-15 21:52:14 +0800

    修改了名字

--- src/main/java/com/zgy/learn/bootswaggermailquartzmongo/BootSwaggerMailQuartzMongoRedisApplication.java
+++ src/main/java/com/zgy/learn/bootswaggermailquartzmongo/BootSwaggerMailQuartzMongoRedisApplication.java
@@ -2,15 +2,13 @@ package com.zgy.learn.bootswaggermailquartzmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 //@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @SpringBootApplication()
-public class BootSwaggerMailQuartzMongoApplication {
+public class BootSwaggerMailQuartzMongoRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootSwaggerMailQuartzMongoApplication.class, args);
+        SpringApplication.run(BootSwaggerMailQuartzMongoRedisApplication.class, args);
     }
 
 }
--- src/test/java/com/zgy/learn/bootswaggermailquartzmongo/BootSwaggerMailQuartzMongoRedisApplicationTests.java
+++ src/test/java/com/zgy/learn/bootswaggermailquartzmongo/BootSwaggerMailQuartzMongoRedisApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BootSwaggerMailQuartzMongoApplicationTests {
+public class BootSwaggerMailQuartzMongoRedisApplicationTests {
 
     @Test
     public void contextLoads() {
