9e2d1cde92b4aa8af25568cfd5c5f24dfa739d25
zuiguangyin <2247359268@qq.com>
2020-04-16 22:17:51 +0800

    swagger设置两个docket

--- src/main/java/com/zgy/learn/bootswaggermailquartzmongo/config/SwaggerConfig.java
+++ src/main/java/com/zgy/learn/bootswaggermailquartzmongo/config/SwaggerConfig.java
@@ -31,4 +31,25 @@ public class SwaggerConfig {
                         .licenseUrl("http://www.baidu.com")
                         .build());
     }
+
+    @Bean
+    public Docket createRestApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .groupName("zgy") // 分组名，默认是default
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(
+                        "com.zgy.learn.bootswaggermailquartzmongo.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger, boot, swagger, mail, quartz, mongo的学习例子")
+                        .description("SpringBoot整合Swagger")
+                        .version("3.8")
+                        .contact(new Contact("最光阴",
+                                "https://www.cnblogs.com/prayjourney/",
+                                "zgy@gmail.com"))
+                        .license("APACHE LICENSE, VERSION 2.0")
+                        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                        .build());
+    }
 }
